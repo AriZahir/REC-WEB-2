024da0cb23bb23a99aa05e2b720cc9000e8c24f6
AriZahir <126253698+AriZahir@users.noreply.github.com>
2023-05-15 11:36:58 -0400

    Add files via upload

---
+++ web2/Tab.php
@@ -0,0 +1,67 @@
+<?php
+$path = 'http://' . $_SERVER["HTTP_HOST"] . '/web2';
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+<meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Sistema Acadêmico</title>
+    <link href="./arquivos/css/bootstrap.min.css" rel="stylesheet">
+    <script src="./arquivos/js/bootstrap.bundle.min.js"></script>
+</head>
+<body>
+    <?php include("./menu.php") ?>
+    <div class="container mt-3">
+        <div class="d-flex"> </div>
+<table summary="Material escolar - Levantamento comparativo de preços.">
+
+</div>
+        <h2>Horário de Funcionamento dos Campus - MS</h2>
+        <table class="table table-bordered" summary="Materiais disponiveis-Informações referentes as materias disponiveis no curso">
+        <caption>Tabela de Horário de Funcionamento dos Campus</caption>
+            <thead>
+                <tr>
+                    <th>Campus</th>
+                    <th>Horario</th>
+                    <th>Atendimentos</th>
+                    
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <th>Aquidauana</th>
+                    <td>07:00 ás 17:00</td>
+                    <td>Cerel / Suap / Secretaria</td>
+                    
+                    
+                </tr>
+                <tr>
+                    <th>Dourados</th>
+                    <td>07:45 às 17:30</td>
+                    <td>Cerel / Suap / Secretaria</td>
+                    
+                    
+                </tr>
+                <tr>
+                    <th>Campo Grande</th>
+                    <td>07:00 às 17:00</td>
+                    <td>Cerel / Suap / Secretaria</td>
+                    
+                    
+                </tr>
+                <tr>
+                    <th>Nova Andradina</th>
+                    <td>08:00 às 18:00</td>
+                    <td>Cerel / Suap / Secretaria</td>
+                    
+                    
+                </tr>
+            </tbody>
+        </table>
+
+
+</body>
+</html>
